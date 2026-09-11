5ba8bb2b15e5f1e3b5861b8f3b90573c7e3b252d
garinarka <jagaddhitajalu@gmail.com>
2026-09-11 15:50:02 +0700

    configure Google OAuth client for production deployment

---
+++ config.js
@@ -0,0 +1,9 @@
+// const GOOGLE_CONFIG = {
+//   clientId:
+//     "309449912546-uo73ql0501klhuh1htdh130gg0su9p2i.apps.googleusercontent.com",
+// };
+
+window.GOOGLE_CONFIG = {
+  clientId:
+    "309449912546-uo73ql0501klhuh1htdh130gg0su9p2i.apps.googleusercontent.com",
+};
